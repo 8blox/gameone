11d2f2504bc0497e0dc0d505c0352a178d06d0c1
tstachura <tom@unoworks.ca>
2016-03-28 12:54:12 -0700

    extra line

--- src/com/eblox/Test.java
+++ src/com/eblox/Test.java
@@ -5,8 +5,9 @@ package com.eblox;
  */
 public class Test
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        System.out.println("hello world");
+        System.out.println( "hello world" );
+        System.out.println( "added another line");
     }
 }
