ba72d4e3cd3dccc2d40a501dbe2682a14e090e4c
Philip Stachura <philip.stachura@unoworks.ca>
2016-04-18 11:21:39 -0700

    Update Log.java
    added basic log methods

--- src/com/eblox/Log.java
+++ src/com/eblox/Log.java
@@ -6,5 +6,24 @@ package com.eblox;
  
 public class Log
 {
+	private string URL;
+	private File file;
+	
+	public static void log(string URL)
+	{
+		this.URL = URL;
+		//create file
+	}
+	
+	public error(String e)
+	{
+		write("ERROR: " + e);
+	}
+	
+	public write(String e
+	{
+		file.append("[" + System.getTime() + "] " + e  + "\n");	
+	}
+
 
 }
